4380d1901ad35f498df3506ffe64605ff2a461ae
Avital Yahel <avital.yahel@gmail.com>
2016-07-31 15:27:33 +0300

    actl+consts: separate path & dir const generation into new function.

--- consts.py
+++ consts.py
@@ -1,1 +1,10 @@
+import os
+
 DB_NAME = 'autolite.db'
+
+
+def get_self_path_dir(this_file: str) -> (str, str, str):
+    SELF_ABS_PATH = os.path.realpath(this_file)
+    SELF_FULL_DIR = os.path.dirname(SELF_ABS_PATH)
+    SELF_SUB_DIR = os.path.basename(SELF_FULL_DIR)
+    return SELF_ABS_PATH, SELF_FULL_DIR, SELF_SUB_DIR
