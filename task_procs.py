0bf8f45ed69d24f3a6e6f18ee1066bb3ab7ba122
Avital Yahel <avital.yahel@gmail.com>
2016-09-18 08:05:03 +0300

    task: new class Task, wrapping db object.

--- task_procs.py
+++ task_procs.py
@@ -1,15 +1,13 @@
 import subprocess
-from datetime import datetime
 
-import db
-from schema import TableSchema
+from task import Task
 from verbosity import verbose
 
 g_procs = {}
 
 
-def start(task: TableSchema):
-    db.update_task(name=task.name, state='running', last=datetime.now())
+def start(task: Task):
+    task.start()
     g_procs.update({task.name: subprocess.Popen(task.command, shell=True, universal_newlines=True)})
     verbose(2, 'proc pool added with:', g_procs[task.name])
 
@@ -18,18 +16,21 @@ def serve(timeout: int) -> int:
     completed = []
 
     for task_name, proc in g_procs.items():
+        task = Task(task_name)
         proc.poll()
 
         if proc.returncode is not None:
-            db.update_task(name=task_name, state='failed' if proc.returncode else 'pending')
+            if proc.returncode:
+                task.fail()
+
+            else:
+                task.reset()
+
             completed += [task_name]
 
-        elif timeout:
-            task = db.read_task(task_name)
-
-            if db.is_timed_out(task, timeout):
-                _terminate_and_fail(task, timeout)
-                completed += [task_name]
+        elif timeout and task.expired(timeout):
+            _terminate_and_fail(task=Task(task_name), timeout=timeout)
+            completed += [task_name]
 
     for task in completed:
         del g_procs[task]
@@ -37,9 +38,8 @@ def serve(timeout: int) -> int:
     return len(g_procs)
 
 
-def _terminate_and_fail(task: TableSchema, timeout: int):
-    verbose(0, 'task', task.name, 'timed-out after', timeout, 'sec - failing!')
-    proc = g_procs[task.name]
-    proc.terminate()
+def _terminate_and_fail(task: Task, timeout: int):
+    verbose(0, 'task', task.name, 'timed-out after', timeout, 'sec - terminating!')
+    g_procs[task.name].terminate()
     verbose(2, 'proc terminated, task:', task.name)
-    db.update_task(name=task.name, state='failed')
+    task.fail()
