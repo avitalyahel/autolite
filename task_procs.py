5f691b58dd4cb600b7f4e2e45288ac2aa87d6a72
Avital Yahel <avital.yahel@gmail.com>
2017-10-23 13:54:56 +0300

    runner: write task output to /var/log/autolite/{db-name}/{task-name}.

--- task_procs.py
+++ task_procs.py
@@ -1,14 +1,19 @@
+import os
 import subprocess
 
+import db
 from task import Task
 from verbosity import verbose
+
+LOG_ROOT = '/var/log/autolite'
+
 
 g_procs = {}
 
 
 def start(task: Task):
     task.start()
-    g_procs.update({task.name: subprocess.Popen('AUTOLITE_TASK_NAME="{}" '.format(task.name) + task.command, shell=True, universal_newlines=True)})
+    g_procs.update({task.name: _new_task_proc(task)})
     verbose(2, 'proc pool added with:', g_procs[task.name])
 
 
@@ -33,9 +38,24 @@ def serve(timeout: int) -> int:
             completed += [task_name]
 
     for task in completed:
+        g_procs[task].stdout.close()
         del g_procs[task]
 
     return len(g_procs)
+
+
+def _new_task_proc(task: Task) -> subprocess.Popen:
+    logfile = open(_new_log_path(task), 'w')
+    result = subprocess.Popen('export AUTOLITE_TASK_NAME="{}" ; '.format(task.name) + task.command, 
+                              shell=True, universal_newlines=True, stdout=logfile, stderr=logfile)
+    result.stdout = logfile
+    return result
+
+
+def _new_log_path(task: Task) -> str:
+    log_path = os.path.join(LOG_ROOT, db.name())
+    os.makedirs(log_path, exist_ok=True)
+    return '{path}/{task_name}.log'.format(path=log_path, task_name=task.name)
 
 
 def _terminate_and_fail(task: Task, timeout: int):
